c93eef812bf24a48d025fe7046b7c7c1879018b9
aristizabal7m <s.aristizabal3@utp.edu.co>
2026-06-08 18:47:55 -0500

    fix CI 3

--- soldarte-frontend/src/app/pages/inicio/inicio.ts
+++ soldarte-frontend/src/app/pages/inicio/inicio.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
